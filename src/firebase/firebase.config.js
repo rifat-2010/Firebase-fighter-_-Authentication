17d6bd0d10d4b86d27af1dc3840a05e91ac35efe
Source-Code-007 <utsho926@gmail.com>
2025-10-15 23:32:26 +0600

    env variable added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAK-WW7-Jhou3Py7bQQGaxJPcqFzzNzp8I",
-  authDomain: "fir-fighter-650bf.firebaseapp.com",
-  projectId: "fir-fighter-650bf",
-  storageBucket: "fir-fighter-650bf.firebasestorage.app",
-  messagingSenderId: "202252046775",
-  appId: "1:202252046775:web:685b87aadbda679d115832",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
